aba162ab310f1610231394810f92ed7b4b9952ed
Gianluca Guglielmo <62102058+gigug@users.noreply.github.com>
2021-05-31 17:18:12 +0200

    Delete html directory

--- html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['advanced_20programming_20exam_2',['Advanced Programming Exam',['../md_exam_instructions.html',1,'']]]
-];
--- html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['advanced_20programming_20exam_27',['Advanced Programming Exam',['../md_exam_instructions.html',1,'']]]
-];
